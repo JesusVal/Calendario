76c4037037f3783b7861385dd2ad7e50e1c3c33b
JesusVal <kzkz98@outlook.com>
2020-05-12 12:15:00 -0500

    Cors mode added

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ fetch('https://daswc.herokuapp.com/api/user',
     method: 'GET',
     headers: {
         'x-auth-user': 'token1'
-    }
+    },
+    mode: 'cors'
 }).then( res => res.json())
 .then( data => {
     console.log(data);
